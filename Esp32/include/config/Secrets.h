623e9fdce3f39d974df6164d1d6cacb160e7930e
Eduardo <barbosa.eduardosan@gmail.com>
2025-11-03 21:18:55 -0300

    fix: stop tracking Secrets.h

--- Esp32/include/config/Secrets.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-#define WIFI_SSID     "VIVIANE"
-#define WIFI_PASSWORD "dududavi"
-
-#define WEB_API_KEY   "AIzaSyAOSA4i5Hnd2VT-1877FGvWrdmdHoFkrMs"
-#define DATABASE_URL  "https://aquario-tcc-default-rtdb.firebaseio.com/"
-#define USER_EMAIL    "eduardo.santos@estudante.iftm.edu.br"
-#define USER_PASS     "abcdef"
