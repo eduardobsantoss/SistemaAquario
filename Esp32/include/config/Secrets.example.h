c35b8d3b8061e12437c2794b94ec9f00f3aa6e8a
Eduardo <barbosa.eduardosan@gmail.com>
2025-11-03 21:21:50 -0300

    fix: stop tracking Secrets.h

---
+++ Esp32/include/config/Secrets.example.h
@@ -0,0 +1,13 @@
+#pragma once
+
+// Wi-Fi
+#define WIFI_SSID        "SEU_SSID_AQUI"
+#define WIFI_PASSWORD    "SUA_SENHA_AQUI"
+
+// Firebase (ajuste ao que seu firmware usa)
+#define FIREBASE_PROJECT_ID      "aquario-tcc"
+#define FIREBASE_DATABASE_URL    "https://aquario-tcc-default-rtdb.firebaseio.com"
+// Se usar REST/database secret/token, deixe placeholders:
+#define FIREBASE_API_KEY         "SUA_API_KEY_SE_NECESSARIO"
+#define FIREBASE_USER_EMAIL      "user@example.com"   // se fizer sign-in por email/senha
+#define FIREBASE_USER_PASSWORD   "senha-super-secreta"
